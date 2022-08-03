abdf7951f6ff2743d910db06c208b9031fc16e54
LLTae2 <taehyun5820@gmail.com>
2022-08-03 16:01:37 +0900

    HandleChange 함수 생성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../style/Header.css"
 
 const Header = () => {
+
+  const [word, setWord] = useState('')
+
+  const HandleChange = (e) => {
+    console.info(e.target.value);
+  }
+
   return (
     <div className='Header'>
+      <div className='Input_box'>
         <label>입력</label>
-        <input></input>
+        <input type='text' onChange={HandleChange}></input>
+      </div>
     </div>
   )
 }
